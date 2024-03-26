07bb646713a63a7a9f4432a44a19cf7d780c74ef
Денис Александров <aie-dinis2007@yandex.ru>
2024-03-26 18:11:57 +0300

    first

---
+++ VirusPropagationSimulatorApp/Service/Extention + UIView.swift
@@ -0,0 +1,34 @@
+//
+//  Extention + UIView.swift
+//  VirusPropagationSimulatorApp
+//
+//  Created by Денис Александров on 26.03.2024.
+//
+
+import UIKit
+
+// MARK: - Add Subview On View
+extension UIView {
+    func addSubviews(_ subviews: UIView...) {
+        subviews.forEach { subview in
+            addSubview(subview)
+        }
+    }
+}
+
+
+// MARK: - Shake Animation
+extension UIView {
+    func shake(for duration: TimeInterval = 0.5,
+               withTranslation translation: CGFloat = 10) {
+        let animator = UIViewPropertyAnimator(duration: duration, dampingRatio: 0.3) {
+            self.transform = CGAffineTransform(translationX: translation, y: 0)
+        }
+
+        animator.addAnimations({ [weak self] in
+            self?.transform = .identity
+        }, delayFactor: 0.2)
+
+        animator.startAnimation()
+    }
+}
---
+++ VirusPropagationSimulatorApp/Service/MatrixHelper.swift
@@ -0,0 +1,24 @@
+//
+//  Helper.swift
+//  VirusPropagationSimulatorApp
+//
+//  Created by Денис Александров on 26.03.2024.
+//
+
+import Foundation
+
+final class MatrixHelper {
+    static let shared: MatrixHelper = .init()
+
+    /**
+     Этот метод генерирует правильную  матрицу логических значений с заданным размером.
+     Матрица будет иметь указанное количество строк и столбцов, где каждый элемент изначально установлен в значение false (ложь).
+     */
+    func makeMatrix(_ number: Int) -> [[Bool]] {
+        let rows = sqrt(Double(number))
+        let columns = sqrt(Double(number))
+        return Array(repeating: Array(repeating: false,
+                                      count: Int(columns)),
+                     count: Int(rows))
+    }
+}
